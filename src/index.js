a15e44839d78b2224474529c2b8ff97f229ad1ad
Michał Grzegorczyk <grzegorczykmichal@yahoo.com>
2020-04-04 18:25:35 +0200

    # Please include custom message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function printUser(firstName, lastName, number, street, code, city, country) {
   console.log(
-    `${firstName} ${lastName} lives at ${number}, ${street}, ${code} in ${city}, ${country}`
+    `${firstName} ${lastName} lives at ${number}, ${street}, ${code} in ${city}, ${country}`,
   );
 }
 printUser(
